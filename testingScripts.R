4822c97e1829d9af740d3f7c9d0e0c7b52cecc0c
Tomaz Rodrigues <tomazrodrigues@tomazs-air.mynetworksettings.com>
2025-02-07 17:14:55 -0500

    finished writing the actual scripts now to do the RMD stuff

--- testingScripts.R
+++ testingScripts.R
@@ -1,13 +1,16 @@
 dt <- function() {
-  #part 1 
-  myData <- read.csv("./activity.csv")
-  myData$date <- as.Date(myRawData$date)
   library(ggplot2)
+  library(chron)
+  library(lubridate)
+  
+  #part 1. Getting and cleaning the data
+  myData <- read.csv("./activity.csv")
+  myData$date <- as.Date(myData$date)
+
   ##running through the intervals to make them reasonable time objects
   ##hate that I have to do this
-  library(chron)
   for(i in 1:length(myData$interval)){
-    stringVal <- myRawData[i, 3]
+    stringVal <- myData[i, 3]
     myString <- ""
     if(nchar(stringVal) == 1){
       myString <- paste("00:0", stringVal, ":00", sep="")
@@ -23,12 +26,10 @@ dt <- function() {
     }  
     myData[i, 3] <- chron(times=myString)
   }
-  #View(myData)
   #end part 1
   
   
   #part 2 
-  library(lubridate)
   myDays <- unique(myData$date)
   myStepsPerDay <- data.frame(date=character(), steps=numeric())
   
@@ -41,6 +42,8 @@ dt <- function() {
   hist(myStepsPerDay$steps, breaks=length(myDays))
   meanSteps <- mean(myStepsPerDay$steps)
   medianSteps <- median(myStepsPerDay$steps)
+  print(paste("mean steps are ", meanSteps, sep=""))
+  print(paste("median steps are ", medianSteps, sep=""))
   #end part 2
   
   #part 3 
@@ -52,9 +55,65 @@ dt <- function() {
     myStepsAtInterval <- data.frame(interval=i, steps=meanSteps)
     myStepsPerInterval <- rbind(myStepsPerInterval, myStepsAtInterval)
   }
+  #end part 3 
+  
+  ## calculating most active interval 
   ggplot(myStepsPerInterval, aes(x=interval, y=steps)) +geom_line() + xlab("")
   myMostActiveIndex <- which.max(myStepsPerInterval$steps)
   myMostActiveInterval <- myStepsPerInterval[myMostActiveIndex, 1]
-  print(myMostActiveInterval)
-  #View(myStepsPerInterval)
+  print(paste("my most active interval is", myMostActiveInterval, sep=" "))
+  
+  #imputing missing values
+  myImputedData <- myData
+  missingRows <- 0 ##calculating Missing Rows
+  for( i in 1:length(myImputedData$steps)){
+    if(is.na(myImputedData[i, 1])){
+      missingRows <- missingRows + 1
+      myImputedSteps <- subset(myStepsPerInterval, chron(interval) == myData[i, 3])[1,2]
+      myImputedData[i,1] <- myImputedSteps
+    }
+  }
+  print(paste("Missing rows are", missingRows, sep=" "))
+  
+  ##recalculating mean and median with newly imputed data
+  myStepsPerDayImputed <- data.frame(date=character(), steps=numeric())
+  myDaysImputed <- unique(myImputedData$date)
+  for(i in myDaysImputed){
+    dataOnMyDay <- subset(myImputedData, date == i)
+    totalStepsOnMyDay <- sum(dataOnMyDay$steps, na.rm=TRUE)
+    mySteps <- data.frame(date=i, steps=totalStepsOnMyDay)
+    myStepsPerDayImputed <- rbind(myStepsPerDay, mySteps)
+  }
+  meanStepsImputed <- mean(myStepsPerDayImputed$steps)
+  medianStepsImputed <- median(myStepsPerDayImputed$steps)
+  print(paste("mean steps w/ imputed data are ", meanStepsImputed, sep=""))
+  print(paste("median steps w/ imputed data are ", medianStepsImputed, sep=""))
+  ##according to this imputing the missing data makes very little difference in the mean and median steps taken
+  hist(myStepsPerDayImputed$steps, breaks=length(myDaysImputed))
+  
+  ##checking to see if each day is a weekend or weekday 
+  myImputedData <- cbind(myImputedData, wday=NA)
+  for( i in 1:length(myImputedData$date)){
+      dayOfTheWeek <- weekdays(as.Date(myImputedData[i, 2]))
+      if (dayOfTheWeek == "Saturday" | dayOfTheWeek == "Sunday"){
+        myImputedData[i, 4] <- "weekend"
+      }
+      else {myImputedData[i, 4] <- "weekday"}
+  }
+  #View(myImputedData)
+  
+  myStepsPerIntervalWday <- data.frame(interval=times(), steps=numeric(), wkday=character())
+  for (i in myTimes){
+    dataAtMyTimeWeekday <- subset(myImputedData, interval == i & wday == "weekday")
+    dataAtMyTimeWeekend <- subset(myImputedData, interval == i & wday == "weekend")
+    meanStepsWeekday <- mean(dataAtMyTimeWeekday$steps, na.rm=TRUE)
+    meanStepsWeekend <- mean(dataAtMyTimeWeekend$steps, na.rm=TRUE)
+    myStepsAtIntervalWeekday <- data.frame(interval=i, steps=meanStepsWeekday, wkday="weekday")
+    myStepsAtIntervalWeekend <- data.frame(interval=i, steps=meanStepsWeekend, wkday="weekend")
+    myStepsPerIntervalWday <- rbind(myStepsPerIntervalWday, myStepsAtIntervalWeekday)
+    myStepsPerIntervalWday <- rbind(myStepsPerIntervalWday, myStepsAtIntervalWeekend)
+  }
+  View(myStepsPerIntervalWday)
+  ggplot(myStepsPerIntervalWday, aes(y=steps, x=interval)) +geom_line() + xlab("") + facet_grid(rows = vars(wkday))
+  
 }
